6b474ecd164eec61e402dd560254063e5ef284ad
Vinícius <viniciusminas_@hotmail.com>
2024-08-27 20:09:03 -0300

    atualiza custo

--- dijkstra.py
+++ dijkstra.py
@@ -1,17 +1,18 @@
+# Exemplo de grafo com custos
 graph = {
     'Rio do Sul': {'Ituporanga': 75, 'Petrolândia': 90, 'Atalanta': 18, 'Laurentino': 30, 'Agronômica': 66, 'Ibirama': 157.5},
     'Ituporanga': {'Rio do Sul': 75, 'Aurora': 26.4, 'Nereu': 90, 'Imbuia': 99, 'Atalanta': 17},
-    'Ibirama': {'Rio do Sul': 157.5, 'Imbuia': 178.2, 'Getúlio': 30, 'Chapadão': 132, 'Nereu': 28},
+    'Ibirama': {'Rio do Sul': 157.5, 'Imbuia': 178.2, 'Getúlio': 30,  'Presidente Getulio': 30},
     'Petrolândia': {'Rio do Sul': 90, 'Atalanta': 18, 'Chapadão': 110, 'Imbuia': 90},
     'Agronômica': {'Rio do Sul': 66, 'Laurentino': 24, 'Trombudo Central': 84, 'Atalanta': 12},
-    'Atalanta': {'Rio do Sul': 18, 'Ituporanga': 17, 'Agronômica': 12},
-    'Imbuia': {'Petrolândia': 90, 'Chapadão': 66, 'Ibirama': 178.2},
-    'Chapadão': {'Petrolândia': 110, 'Imbuia': 66, 'Ibirama': 132},
-    'Aurora': {'Ituporanga': 26.4},
-    'Nereu': {'Ituporanga': 90, 'Ibirama': 28},
-    'Laurentino': {'Rio do Sul': 30, 'Agronômica': 24},
+    'Atalanta': {'Rio do Sul': 18, 'Ituporanga': 17, 'Agronômica': 12, 'Petrolandia': 18},
+    'Imbuia': {'Petrolândia': 90, 'Chapadão': 66, 'Ibirama': 178.2, 'Ituporanga': 99},
+    'Chapadão': {'Petrolândia': 110, 'Imbuia': 66, 'Presidente Nereu': 132},
+    'Aurora': {'Agronomica': 22, 'Ituporanga': 26.4}
+    'Presidente Nereu': {'Chapadão do Lageado': 132, 'Ituporanga': 90},
+    'Laurentino': {'Agronomica': 24, 'Dona Emma': 75, 'Rio do Sul': 30},
     'Trombudo Central': {'Agronômica': 84},
-    'Getúlio': {'Ibirama': 30, 'José': 99},
-    'José': {'Getúlio': 99},
-    'Dona Ema': {'Laurentino': 75}
+    'Presidente Getúlio': {'Ibirama': 30, 'José Boiteux': 99},
+    'José Boiteux': {'Presidente Getúlio': 99},
+    'Dona Emma': {'Laurentino': 75}
 }

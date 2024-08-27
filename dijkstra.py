64403386e827fd7ac354fa247d1c254333dbb389
Vinícius <viniciusminas_@hotmail.com>
2024-08-27 19:47:07 -0300

    test

---
+++ dijkstra.py
@@ -0,0 +1,17 @@
+graph = {
+    'Rio do Sul': {'Ituporanga': 75, 'Petrolândia': 90, 'Atalanta': 18, 'Laurentino': 30, 'Agronômica': 66, 'Ibirama': 157.5},
+    'Ituporanga': {'Rio do Sul': 75, 'Aurora': 26.4, 'Nereu': 90, 'Imbuia': 99, 'Atalanta': 17},
+    'Ibirama': {'Rio do Sul': 157.5, 'Imbuia': 178.2, 'Getúlio': 30, 'Chapadão': 132, 'Nereu': 28},
+    'Petrolândia': {'Rio do Sul': 90, 'Atalanta': 18, 'Chapadão': 110, 'Imbuia': 90},
+    'Agronômica': {'Rio do Sul': 66, 'Laurentino': 24, 'Trombudo Central': 84, 'Atalanta': 12},
+    'Atalanta': {'Rio do Sul': 18, 'Ituporanga': 17, 'Agronômica': 12},
+    'Imbuia': {'Petrolândia': 90, 'Chapadão': 66, 'Ibirama': 178.2},
+    'Chapadão': {'Petrolândia': 110, 'Imbuia': 66, 'Ibirama': 132},
+    'Aurora': {'Ituporanga': 26.4},
+    'Nereu': {'Ituporanga': 90, 'Ibirama': 28},
+    'Laurentino': {'Rio do Sul': 30, 'Agronômica': 24},
+    'Trombudo Central': {'Agronômica': 84},
+    'Getúlio': {'Ibirama': 30, 'José': 99},
+    'José': {'Getúlio': 99},
+    'Dona Ema': {'Laurentino': 75}
+}
